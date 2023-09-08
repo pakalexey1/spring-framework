42f1073b79c74f9226d1d8e026f4aa76f9b1d999
pakalexey <mye604@gmail.com>
2023-09-08 14:24:48 -0400

    Spring-06 basic structure complete and application from Spring-04 copied and adapted to the new structure.

--- Spring-06-SpringBoot-Demo/src/main/java/org/example/ExampleApplication.java
+++ Spring-06-SpringBoot-Demo/src/main/java/org/example/ExampleApplication.java
@@ -1,13 +1,27 @@
 package org.example;
 
+import org.example.model.Comment;
+import org.example.service.CommentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = SpringApplication.run(ExampleApplication.class, args);
+
+        CommentService cs = context.getBean(CommentService.class);
+        cs.publishComment(comment);
+
+
     }
 
 }
---
+++ Spring-06-SpringBoot-Demo/src/main/java/org/example/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,14 @@
+package org.example.proxy;
+
+import org.example.model.Comment;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment){
+        System.out.println("Sending notification for comment: " + comment.getText());
+    }
+}
