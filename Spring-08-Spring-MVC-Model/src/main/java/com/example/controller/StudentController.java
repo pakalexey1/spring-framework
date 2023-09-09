0b1e72d70f0b1a749cfb4d9adf02fdc0933765ed
pakalexey <mye604@gmail.com>
2023-09-09 16:55:57 -0400

    Spring Controller complete String Model completed (not Maven)

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -1,13 +1,20 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homepage(){
+    public String homepage(Model model){
+
+    model.addAttribute("name","Welcome to");
+    model.addAttribute("course","MVC course");
+
+
+
         return "/student/welcome";
     }
 }
