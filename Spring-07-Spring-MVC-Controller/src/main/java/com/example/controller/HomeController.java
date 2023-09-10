e8f88919d7dab9e8c84a4fcfbfdfd52efb2853bc
pakalexey <mye604@gmail.com>
2023-09-09 22:46:29 -0400

    Spring 08 MVC Model Completed

--- Spring-07-Spring-MVC-Controller/src/main/java/com/example/controller/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/example/controller/HomeController.java
@@ -11,8 +11,8 @@ public class HomeController {
     public String home(){
         return "home.html";
     }
-    @RequestMapping("/welcome")
-    public String welcome(){
-        return "welcome.html";
-    }
+//    @RequestMapping("/welcome")
+//    public String welcome(){
+//        return "welcome.html";
+//    }
 }
---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/MentorController.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import com.example.enums.Gender;
+import com.example.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @RequestMapping("/list")
+    public String mentor(Model model){
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike","Smith",45, Gender.MALE));
+        mentorList.add(new Mentor("Tom","Hanks",65, Gender.MALE));
+        mentorList.add(new Mentor("Ammy","Bryan",25, Gender.FEMALE));
+
+        model.addAttribute("mentors",mentorList);
+
+
+        Mentor mentor1 = new Mentor("Mike","Smith",45, Gender.MALE);
+        model.addAttribute("mike",mentor1);
+        Mentor mentor2= new Mentor("Tom","Hanks",65, Gender.MALE);
+        model.addAttribute("tom",mentor2);
+        Mentor mentor3 = new Mentor("Ammy","Bryan",25, Gender.FEMALE);
+        model.addAttribute("ammy",mentor3);
+
+        return "/mentor/mentor";
+    }
+}
