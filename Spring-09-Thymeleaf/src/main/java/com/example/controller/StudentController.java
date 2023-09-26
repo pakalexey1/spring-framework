64ee2f8dd62740ad16bf191941cdecb650bf6a67
pakalexey <mye604@gmail.com>
2023-09-25 22:07:49 -0400

    Created Mentor form application

--- Spring-09-Thymeleaf/src/main/java/com/example/controller/StudentController.java
+++ Spring-09-Thymeleaf/src/main/java/com/example/controller/StudentController.java
@@ -3,19 +3,23 @@ package com.example.controller;
 import com.example.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+//    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping(value="/register")
     public String register(Model model){
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+//    @RequestMapping("/welcome", method = RequestMethod.GET)
+    @GetMapping("/welcome")
     public String info(){
         return "student/welcome";
     }
