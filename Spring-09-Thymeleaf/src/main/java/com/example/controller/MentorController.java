9287c5be9c4e2620c98154a9bdceacd1fe1011f1
pakalexey <mye604@gmail.com>
2023-09-26 14:34:14 -0400

    Finished creating Mentor form application

--- Spring-09-Thymeleaf/src/main/java/com/example/controller/MentorController.java
+++ Spring-09-Thymeleaf/src/main/java/com/example/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.example.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -22,6 +24,15 @@ public class MentorController {
 
         return "mentor/mentor-register";
 
+    }
+
+    @PostMapping("confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor){
+
+
+//        return "mentor/mentor-confirmation";
+
+        return "redirect:/mentor/register";
     }
 
 }
