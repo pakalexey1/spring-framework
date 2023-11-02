dbbc7a8b01c9171744dc53ef2d5abc8be5bd0c1a
pakalexey <mye604@gmail.com>
2023-11-01 21:14:19 -0400

    Spring 13 started Created Movie table Created enums

--- Spring-12-ORM-Mapping-OneToMany-Many/src/main/java/com/example/bootstrap/DataGenerator.java
+++ Spring-12-ORM-Mapping-OneToMany-Many/src/main/java/com/example/bootstrap/DataGenerator.java
@@ -1,25 +1,29 @@
 package com.example.bootstrap;
 
 import com.example.enums.Status;
-import com.example.model.Merchant;
-import com.example.model.Payment;
-import com.example.model.PaymentDetail;
-import com.example.repository.MerchantRepository;
-import com.example.repository.PaymentRepository;
+import com.example.model.*;
+import com.example.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
     private final PaymentRepository paymentRepository;
     private final MerchantRepository merchantRepository;
+    private final CustomerRepository customerRepository;
+    private final CartRepository cartRepository;
+    private final ItemRepository itemRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository, CustomerRepository customerRepository, CartRepository cartRepository, ItemRepository itemRepository) {
         this.paymentRepository = paymentRepository;
         this.merchantRepository = merchantRepository;
+        this.customerRepository = customerRepository;
+        this.cartRepository = cartRepository;
+        this.itemRepository = itemRepository;
     }
 
 
@@ -38,8 +42,37 @@ public class DataGenerator implements CommandLineRunner {
 
         Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
 
+        Customer customer1 = new Customer("msmith", "Mike", "Smith","msith@cydeo.com","VA");
+
+        payment1.setCustomer(customer1);
+        payment2.setCustomer(customer1);
+
+        customerRepository.save(customer1);
+
         payment1.setMerchant(merchant1);
         payment2.setMerchant(merchant1);
+
+        Item item1 = new Item("Milk", "M01" );
+        Item item2 = new Item("Sugar", "S01" );
+        Item item3 = new Item("Bread", "B01" );
+
+        Cart cart1 = new Cart();
+        Cart cart2 = new Cart();
+
+        cart1.setItemList(Arrays.asList(item1,item2,item3));
+        cart2.setItemList(Arrays.asList(item1,item2));
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        itemRepository.save(item3);
+
+        cartRepository.save(cart1);
+        cartRepository.save(cart2);
+
+
+
+
+
 
         merchantRepository.save(merchant1);
 
