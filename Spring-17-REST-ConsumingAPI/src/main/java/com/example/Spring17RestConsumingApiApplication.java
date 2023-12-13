e33f8ac9ce3c066352abc373f7b2e9d2d59fafdf
pakalexey <mye604@gmail.com>
2023-12-13 16:16:06 -0500

    Started Spring 17

---
+++ Spring-17-REST-ConsumingAPI/src/main/java/com/example/Spring17RestConsumingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring17RestConsumingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumingApiApplication.class, args);
+    }
+
+}
