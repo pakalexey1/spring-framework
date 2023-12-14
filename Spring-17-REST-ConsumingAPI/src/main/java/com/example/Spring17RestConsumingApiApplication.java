0f3e1fc658c0df8d3b368dc8ddc9624d8d40554f
pakalexey <mye604@gmail.com>
2023-12-13 21:44:18 -0500

    Created DTO classes to consume API

--- Spring-17-REST-ConsumingAPI/src/main/java/com/example/Spring17RestConsumingApiApplication.java
+++ Spring-17-REST-ConsumingAPI/src/main/java/com/example/Spring17RestConsumingApiApplication.java
@@ -3,12 +3,14 @@ package com.example;
 import com.example.dto.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Spring17RestConsumingApiApplication {
 
     public static void main(String[] args) {
--- Spring-17-REST-ConsumingAPI/src/main/java/com/example/controller/Consume_RestTemplate.java
+++ Spring-17-REST-ConsumingAPI/src/main/java/com/example/controller/Consume_RestTemplate.java
@@ -2,13 +2,14 @@ package com.example.controller;
 
 import com.example.dto.User;
 import org.apache.coyote.Response;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 //Old approach - not used anymore
@@ -35,6 +36,20 @@ public class Consume_RestTemplate {
     public Object readUser(@PathVariable("id") Integer id){
         String URL = URI + "/{id}";
         return restTemplate.getForObject(URL,Object.class,id);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET, entity, Object.class);
+
+        return response;
+
     }
 
 
